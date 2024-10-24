e14e7599bdc9e9f25bf4e43ca12c741225acfef2
Musila Peter <95099676+peter4Xp@users.noreply.github.com>
2024-10-24 13:19:27 +0300

    Add files via upload
    A simple site to use in the meantime

---
+++ script.js
@@ -0,0 +1,9 @@
+function sendMessage() {
+    const input = document.getElementById("chat-input");
+    const chatBox = document.getElementById("chat-box");
+    
+    if (input.value.trim() !== "") {
+        const message = document.createElement("div");
+        message.textContent
+       }
+     }
